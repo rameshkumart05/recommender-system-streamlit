cf674dd8051ff86663c04cc16e214058a1d2a940
George Perdrizet <george@perdrizet.org>
2025-03-31 19:28:12 +0000

    Added script to get things set up on Render server.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Helper script to set up movie recommendation
+# service on render. Makes directory structure,
+# installs Python dependencies and runs python 
+# script to train model.
+
+# Make asset directories
+mkdir data
+mkdir models
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Train the model
+python ./src/train_model.py
