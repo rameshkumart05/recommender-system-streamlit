4d6e544020b79b8f8b13ebd4ace37903c6f644e7
George Perdrizet <george@perdrizet.org>
2025-04-02 18:41:32 +0000

    Completed basic web app, including try/except to catch movies that are not in the database.

--- src/app.py
+++ src/app.py
@@ -1,40 +1,53 @@
-'''Simple command line utility to make movie recommendations based on KNN with cosine similarity.'''
+'''Movie recommendation web app with Flask.'''
 
 import pickle
+from flask import Flask, request, render_template
+
+# Define the flask application
+app=Flask(__name__)
+
+# Load the assets
+MODEL=pickle.load(open('../models/model.pkl', 'rb'))
+TFIDF_MATRIX=pickle.load(open('../data/tfidf_matrix.pkl', 'rb'))
+ENCODED_DATA_DF=pickle.load(open('../data/encoded_features_df.pkl', 'rb'))
+
 
 def get_movie_recommendations(movie_title, model, tfidf_matrix, encoded_data_df):
     '''Takes a movie title string, looks up TFIDF feature vector for that movie
     and returns title of top 5 most similar movies'''
 
     # Find the query movie in the encoded data, get the index
-    movie_index = encoded_data_df[encoded_data_df["title"] == movie_title].index[0]
+    try:
+        movie_index=encoded_data_df[encoded_data_df['title'] == movie_title].index[0]
 
-    # Get the distances and indexes of similar movies
-    distances, indices = model.kneighbors(tfidf_matrix[movie_index])
+    except IndexError:
+        return 'Movie not found'
+
+    # Get the indexes of similar movies
+    _, indices=model.kneighbors(tfidf_matrix[movie_index])
 
     # Extract the titles of the similar movie
-    similar_movies = [(encoded_data_df["title"][i], distances[0][j]) for j, i in enumerate(indices[0])]
+    similar_movies=[encoded_data_df['title'][i] for i in indices[0]]
     
     return similar_movies[1:]
 
 
-if __name__ == '__main__':
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
 
-    # Load the assets
-    model=pickle.load(open("../models/model.pkl", "rb"))
-    tfidf_matrix=pickle.load(open("../data/tfidf_matrix.pkl", "rb"))
-    encoded_data_df=pickle.load(open("../data/encoded_features_df.pkl", "rb"))
+        input_movie=request.form['title']
+        recommendations=get_movie_recommendations(
+            input_movie,
+            MODEL,
+            TFIDF_MATRIX,
+            ENCODED_DATA_DF
+        )
 
-    # Loop until user breaks
-    while True:
+        print(f'User input: {input_movie}')
+        print(f'Result: {recommendations}')
 
-        # Get a movie title from the user
-        input_movie=input("Enter a movie title: ")
+    else:
+        recommendations=None
 
-        # Get the recommendations
-        recommendations=get_movie_recommendations(input_movie, model, tfidf_matrix, encoded_data_df)
-
-        # Print the results
-        print("Film recommendations '{}'".format(input_movie))
-        for movie, distance in recommendations:
-            print("- Film: {}".format(movie))
+    return render_template('index.html', recommendations=recommendations)
