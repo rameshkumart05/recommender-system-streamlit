7107de1d533a81600441ac862b115f6a9d5a3401
George Perdrizet <george@perdrizet.org>
2025-05-07 17:32:40 +0000

    Added __init__.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,8 +5,7 @@ import movie_recommender.movie_recommender as recommender
 def test_model_output():
     '''Tests to see if the model is giving sane output.'''
 
-    # Call for recommendations with an input for which we know the
-    # expected output
+    # Call then recommendation function with an input for which we know the expected output
     similar_movies=recommender.get_movie_recommendations(
         'Star Wars',
         recommender.MODEL,
@@ -14,4 +13,5 @@ def test_model_output():
         recommender.ENCODED_DATA_DF
     )
 
+    # Check that we received the expected answer
     assert similar_movies[0] == 'The Empire Strikes Back'
